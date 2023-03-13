056b697c66ad67b45d4c456c155d7b4f09f4adad
Stelian-Constantin Stanciu <stanciu.stelian86@yahoo.fr>
2023-03-13 14:42:33 +0100

    ahout class Fruit

--- index.php
+++ index.php
@@ -5,29 +5,35 @@ class Voiture
     //public $couleur='rouge';
     //private $couleur='rouge';
     private $couleur;
-    public static $jante='metal';
+    public $vitesse;
+    public static $jante = 'metal';
 
     public function __construct()
     {
         //echo 'Construct. ';
     }
-   public function setCouleur($couleurProp){
-    $this->couleur=$couleurProp;
-   }
-   public function getCouleur(){
-    return $this->couleur;
-   }
-    public function acceler()
+    public function setCouleur($couleurProp)
     {
-       // echo 'La voiture accelere';
-        return 'Vroum!';
+        $this->couleur = $couleurProp;
     }
+    public function getCouleur()
+    {
+        return $this->couleur;
+    }
+    public function acceler($vitesseProp): int
+    {
+        // echo 'La voiture accelere';
+        //return 'Vroum!';
+       return $this->vitesse = $vitesseProp + 10;
+    }
+
     public static function accelerStatic()
     {
         return 'Vroum Static!';
     }
-    public function __destruct(){
-        echo 'Couleur de la voiture: ' . $this->couleur;
+    public function __destruct()
+    {
+        //echo 'Couleur de la voiture: ' . $this->couleur;
     }
 };
 // il faut créer une fonction pour pouvoir acceder au varbiable 'private' 
@@ -41,7 +47,24 @@ class Voiture
 //echo 'Ma voiture a ' . Voiture::ROUE . ' roues.';
 //echo Voiture::$jante;
 //echo Voiture::accelerStatic();
-$sport=new Voiture();
-$sport->setCouleur('Jaune');
-echo $sport->getCouleur();
+// $sport=new Voiture();
+// $sport->setCouleur('Jaune');
+// echo $sport->getCouleur();
 
+// $sport = new Voiture();
+// echo $sport->acceler(30);
+
+class Fruit
+{
+    private $nomFruit;
+    public function __get($nom){
+       // return "L'attribut (propriete) $nom est innacessible";
+        return $this->nomFruit;
+    }
+    public function __set($nom,$val){
+        $this ->$nom=$val;
+    }
+}
+$pomme=new Fruit();
+$pomme->nomFruit='eat eat eat' . ' Pomme Golden';
+echo $pomme->nomFruit;
